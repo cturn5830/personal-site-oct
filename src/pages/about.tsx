c45d884530db4a226601658811666df372acf068
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2023-10-24 19:31:22 -0400

    Text updates

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -31,23 +31,28 @@ export default function About() {
         <section className={styles.section1}>
           <span className={styles.list}>
             <div>
-              <h2>Why I'm a Developer.</h2>
+              <h2 className={styles.title}>Why I'm a Developer.</h2>
               <p>
-                Growing up I always wanted my devices to do particular things,
-                however I didn't always have the best things, so I made it work.
-                I would teach myself how to install new apps on outdated phones,
-                jailbreak game systems, and even build my own gameboy-like hand
-                held. Without knowing I’ve learned when there's a will, there's
-                a way, and many of these skills have translated directly into
-                being a web-developer.
+                Growing up, I always wanted my devices to do particular things.
+                However, I didn't always have the best hardware; so, I would
+                find ways to make it work. I taught myself how to install new
+                apps on outdated phones, jailbreak video game systems, and even
+                built my own Gameboy-like hand held device. Without even knowing
+                it, I lived the old adage “where there's a will, there's a way.”
+                In turn, many of those skills I picked up along the way have
+                translated directly into useful tools in my work as a
+                web-developer.
               </p>
             </div>
             <div>
-              <h2>My Goal.</h2>
+              <h2 className={styles.title}>My Goal.</h2>
               <p>
-                To solve problems and make life easier for businesses and
-                consumers alike though the internet. Helping others though
-                websites, ultimately helps myself as well.
+                I aim to solve problems and make life easier, for businesses and
+                consumers alike, through the internet. By continuing to home my
+                craft through web development, I learn more and am able to
+                provide even better results for my clients and their customers
+                as well. By helping others through web development, I find my
+                own fulfillment as well.
               </p>
             </div>
           </span>
@@ -59,27 +64,28 @@ export default function About() {
               <li>
                 <h3>Serving With Christ.</h3>
                 <p>
-                  Serving with Christ is working with an open heart and moving
-                  in a super-natural way, fulfilling his will through actions
-                  and behaviours. This also means serving others with a giving
-                  heart, being generous and kind.
+                  Serving with Christ means working with an open heart and
+                  moving in a supernatural way, furthering His teachings through
+                  actions and behaviors. This also means serving others with a
+                  giving heart, being generous, and displaying kindness.
                 </p>
               </li>
               <li>
                 <h3>Working Openly.</h3>
                 <p>
-                  No one wants to just hope that something is getting done, I’m
-                  about communicating where I’m at with your project and keeping
-                  everyone up to date on everything.
+                  No one wants to just hope that something is getting done. I am
+                  committed to regularly communicating with you about what step
+                  of the process I am at and keeping you up to date on the
+                  status of your project.
                 </p>
               </li>
               <li>
                 <h3>Keeping All Cards On The Table.</h3>
                 <p>
-                  Keeping all cards on the table means being open about what you
-                  can do and having all options available, and providing
+                  Nobody likes being sold an empty promise. I will be open about
+                  the options available to you, what you can do, and provide
                   understanding of the proper resources to complete the task via
-                  images or text
+                  images or text.
                 </p>
               </li>
               <li>
@@ -87,7 +93,7 @@ export default function About() {
                 <p>
                   I’m a huge user of notes and love to jot down big facts down
                   key elements to look back on, this avoids missing elements in
-                  the future and minimizing revisions, helping myself and you
+                  the future and minimizing revisions, helping myself and you.
                 </p>
               </li>
             </ul>
